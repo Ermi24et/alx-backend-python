ae0c7a9ea66d1ace2559864f9a3bb56ecc799ab5
Ermi24et <lilermi24@gmail.com>
2024-03-15 03:38:06 +0300

    floor division

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""a type-annotated function that calculates floor of a number"""
+import math
+
+
+def floor(n: float) -> int:
+    """a function that returns a float of a number"""
+    return math.floor(n)
