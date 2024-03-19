446514451be57e5f8a940837abe2835bf034ec61
Ermi24et <lilermi24@gmail.com>
2024-03-19 14:38:39 +0300

    async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+a module that contains coroutine
+"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator():
+    """
+    a coroutine that yields a random number
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
