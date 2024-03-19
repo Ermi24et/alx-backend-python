b11c7fec5916c720d5117f1e735f95429f740ff5
Ermi24et <lilermi24@gmail.com>
2024-03-19 14:58:23 +0300

    async comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a module of coroutine
+"""
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    a coroutine that uses async comprehension over async generator
+    """
+    return [n async for n in async_generator()]
