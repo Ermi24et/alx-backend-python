abd1daaae64893e3c6e8a82dd4ef0455f412cbad
Ermi24et <lilermi24@gmail.com>
2024-04-06 17:29:28 +0300

    parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,7 +21,8 @@ class TestAccessNestedMap(unittest.TestCase):
         [{"a": {"b": 2}}, ("a",), 2],
         [{"a": {"b": 2}}, ("a", "b"), 2]
     ])
-    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected: int) -> Any:
+    def test_access_nested_map(self, nested_map: Mapping, path: Sequence,
+                               expected: int) -> Any:
         """
         test method to test if a functions returns the desired output
         """
